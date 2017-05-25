3afd00704d65d29a74a985fcf5deb194f85c40fb
Maxime Delpit <maximdelpit@gmail.com>
2017-05-25 15:10:33 +0200

    postmark config

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   after_create :send_welcome_email
+  after_create :subscribe_to_newsletter
 
   private
 
   def send_welcome_email
     UserMailer.welcome(self).deliver_now
+  end
+
+  def subscribe_to_newsletter
+    SubscribeToNewsletterService.new(self).call
   end
 end
